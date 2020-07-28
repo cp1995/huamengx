898a099e0026016ea028831b69fd744b95a18f2f
cp <1208202418@qq.com>
2020-07-28 08:31:42 +0800

    区域

---
+++ dd-web-war/src/main/java/com/cp/dd/web/controller/member/ZsCategoryController.java
@@ -0,0 +1,24 @@
+package com.cp.dd.web.controller.member;
+
+import io.swagger.annotations.Api;
+import lombok.AllArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author CodeGenerator
+ * @date 2020-07-23
+ */
+@Validated
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/zsCategory")
+@Api(value = "/zsCategory", tags = "")
+public class ZsCategoryController {
+
+}
