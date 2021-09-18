baae105cb79c0b71dbcdb4e963a494f63c4184eb
程鹏 <1208202418@qq.com>
2021-09-18 18:20:46 +0800

    修改

--- dd-common/src/main/java/com/cp/dd/common/support/validator/custom/Mobile.java
+++ dd-common/src/main/java/com/cp/dd/common/support/validator/custom/Mobile.java
@@ -26,7 +26,7 @@ public @interface Mobile {
      */
     String message() default "手机格式不正确";
 
-    String pattern() default "^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\\d{8}$";
+    String pattern() default "^1([39][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\\d{8}$";
 
     Class<?>[] groups() default {};
 
